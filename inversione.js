846c075ce62a44cb6bd32e99bdcea95286be235d
Nicola Memo <n.memo@donboscosandona.it>
2024-01-19 12:42:59 +0100

    sefs

--- inversione.js
+++ inversione.js
@@ -1,23 +1,23 @@
-// Definizione di una funzione chiamata string_reverse con un parametro denominato str
-function string_reverse(str) {
+// Definizione di una funzione chiamata inversione_stringa con un parametro denominato str
+function inversione_stringa(str) {
     // Converte la stringa in un array di singoli caratteri
-    var charactersArray = str.split("");
+    var vettoreCaratteri = str.split("");
     
     // Inverte l'ordine degli elementi all'interno dell'array
-    var reversedArray = charactersArray.reverse();
+    var vettoreInertito = vettoreCaratteri.reverse();
     
     // Unisce gli elementi dell'array per formare una stringa invertita
-    var reversedString = reversedArray.join("");
+    var stringaInvertita = vettoreInertito.join("");
     
     // Restituisce la stringa invertita come risultato della funzione
-    return reversedString;
+    return stringaInvertita;
 }
 
-// Stampa il risultato della chiamata alla funzione string_reverse con l'argomento "w3resource" sulla console
-console.log("Risultato di string_reverse(\"w3resource\"): ", string_reverse("w3resource"));
+// Stampa il risultato della chiamata alla funzione inversione_stringa con l'argomento "w3resource" sulla console
+console.log("Risultato di inversione_stringa(\"w3resource\"): ", inversione_stringa("w3resource"));
 
-// Stampa il risultato della chiamata alla funzione string_reverse con l'argomento "www" sulla console
-console.log("Risultato di string_reverse(\"www\"): ", string_reverse("www"));
+// Stampa il risultato della chiamata alla funzione inversione_stringa con l'argomento "www" sulla console
+console.log("Risultato di inversione_stringa(\"www\"): ", inversione_stringa("www"));
 
-// Stampa il risultato della chiamata alla funzione string_reverse con l'argomento "JavaScript" sulla console
-console.log("Risultato di string_reverse(\"JavaScript\"): ", string_reverse("JavaScript"));
+// Stampa il risultato della chiamata alla funzione inversione_stringa con l'argomento "JavaScript" sulla console
+console.log("Risultato di inversione_stringa(\"JavaScript\"): ", inversione_stringa("JavaScript"));
